5f78bf5dd25236a1a0d750b64a3feb7943b6d8aa
S <s>
2017-06-08 13:31:06 +0100

    {"types":"C","runs":30}

--- Breakout.java
+++ Breakout.java
@@ -106,7 +106,7 @@ public class Breakout extends GraphicsProgram {
 	public void init() {
 		setSize(WIDTH, HEIGHT); 
 		double x = (WIDTH -PADDLE_WIDTH)/2;
-		double y = (HEIGHT - PADDLE_Y_OFFSET - PADDLE_HEIGHT*2');
+		double y = (HEIGHT - PADDLE_Y_OFFSET - PADDLE_HEIGHT*2);
 		GRect paddle = new GRect(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
 		paddle.setFilled(true);
 		add(paddle);
