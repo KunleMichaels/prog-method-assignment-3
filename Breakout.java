b673346a2a2f61591c04189fc512b777e999ee82
S <s>
2017-06-09 20:55:48 +0100

    {"types":"C","runs":21}

--- Breakout.java
+++ Breakout.java
@@ -190,7 +190,7 @@ public class Breakout extends GraphicsProgram {
 	}
 	
 	private void collidewithPaddle() {
-		if (ball.getX() == paddle.getX() && ball.getY() == paddle.getY()){
+		if (ball.getX() == paddle.getX() || ball.getY() == paddle.getY()){
 		ball.move(vx, -vy);
 		}
 		
