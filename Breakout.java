71fb634ba6cfa5797d639eca4a584c6174d36125
S <s>
2017-06-09 13:42:15 +0100

    {"types":"C","runs":16}

--- Breakout.java
+++ Breakout.java
@@ -118,7 +118,7 @@ public class Breakout extends GraphicsProgram {
 		if (e.getX() >= 0 && e.getX() < (WIDTH - PADDLE_WIDTH)){
 			paddle.setLocation(e.getX(), getHeight() - PADDLE_Y_OFFSET - PADDLE_HEIGHT);
 		} else if (e.getX() >= WIDTH) {
-			paddle.setLocation(WIDTH - PADDLE_WIDTH, HEIGHT * PADDLE_Y_OFFSET);
+			paddle.setLocation(WIDTH - PADDLE_WIDTH, 0);
 		}
 		
 		
